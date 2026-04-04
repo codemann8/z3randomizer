b17c818f0a8f4c9b48514c262387a296984e366b
codemann8 <codemann8@gmail.com>
2026-04-04 11:57:00 -0500

    Fix false mirror portal on ped screen

--- owrando.asm
+++ owrando.asm
@@ -302,6 +302,7 @@ OWMirrorSpriteDisable:
 	    PLA : PLA : PLA : JML Sprite_6C_MirrorPortal_missing_mirror
 	+ 
     
+    lda.b OverworldIndex : and.b #$80 : bne +
     lda.l OWMode+1 : and.b #!FLAG_OW_CROSSED : beq .vanilla
         lda.l InvertedMode : beq +
             lda.b #$40
